7b9380ad128d8002982e02453ce7397f41d527e8
illia <illia.pikin@gmail.com>
2023-11-24 20:44:43 +0200

    added CSI camera builder

---
+++ go/gstpipeline/builder.go
@@ -0,0 +1,72 @@
+package gstpipeline
+
+import "fmt"
+
+const SENSORS_DSP_PATH string = "/opt/imaging"
+
+type Sensor string
+
+const (
+	IMX219 Sensor = "imx219"
+	IMX390 Sensor = "imx390"
+)
+
+func CsiCameraSetup(sensor Sensor, index uint, width uint, height uint) string {
+	var fullName string
+	switch sensor {
+	case IMX219:
+		switch index {
+		case 0:
+			fullName = "imx219 6-0010"
+		case 1:
+			fullName = "imx219 4-0010"
+		}
+		// todo IMX390
+	}
+	return fmt.Sprintf("media-ctl -d %d --set-v4l2 '\"%s\":0[fmt:SRGGB8_1X8/%dx%d]'",
+		index, fullName, width, height)
+}
+
+func GStreamerLaunch() string {
+	return "gst-launch-1.0"
+}
+
+func CsiCameraSource(sensor Sensor, index uint, width uint, height uint) string {
+	var device string
+	var subdev string
+	var sensorName string
+	var formatMsb uint
+	switch sensor {
+	case IMX219:
+		sensor = "SENSOR_SONY_IMX219_RPI"
+		formatMsb = 7
+	case IMX390:
+		sensor = "IMX390-UB953_D3"
+		formatMsb = 11
+	}
+	switch index {
+	case 0:
+		device = "/dev/video2"
+		subdev = "/dev/v4l-subdev2"
+	case 1:
+		device = "/dev/video18"
+		subdev = "/dev/v4l-subdev5"
+	}
+	return fmt.Sprintf("v4l2src device=%s ! video/x-bayer, width=%d, height=%d, format=rggb ! tiovxisp sink_0::device=%s sensor-name=%s dcc-isp-file=%s/%s/dcc_viss.bin sink_0::dcc-2a-file=%s/%s/dcc_2a.bin format-msb=%d",
+		device, width, height, subdev, sensorName, SENSORS_DSP_PATH, sensor, SENSORS_DSP_PATH, sensor, formatMsb)
+}
+
+func TestSource(width uint, height uint) string {
+	return fmt.Sprintf("videotestsrc ! video/x-raw, width=%d, height=%d",
+		width, height)
+}
+
+func DecodeBinRescale(width uint, height uint) string {
+	return fmt.Sprintf("! decodebin ! videoscale method=0 add-borders=false ! video/x-raw, width=%d, height=%d",
+		width, height)
+}
+
+func JpegTcpStreamLocalhost(quality uint, port uint, boundary string) string {
+	return fmt.Sprintf("! jpegenc quality=%d ! multipartmux boundary=%s ! tcpclientsink host=127.0.0.1 port=%d",
+		quality, boundary, port)
+}
--- go/mjpeg_http_mux.go
+++ go/mjpeg_http_mux.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"bbai64/gstpipeline"
 	"bbai64/muxer"
 	"bufio"
 	"io"
 	"log"
 	"net"
 	"net/http"
+	"os/exec"
 	"time"
 )
 
 const SERVER_ADDRESS = ":1337"
-const CHUNKS_BUFFER_SIZE = 16
+const CHUNKS_BUFFER_SIZE = 128
 const MJPEG_FRAME_BOUNDARY = "frameboundary"
 const CONNECTION_TIMEOUT = 1 * time.Second
+const CAMERA_WIDTH = 1920
+const CAMERA_HEIGHT = 1080
+const RESCALE_WIDTH = 1280
+const RESCALE_HEIGHT = 720
+const JPEG_QUALITY = 80
 
 func serveTcpSocket(mux *muxer.Muxer, address string) {
 	soc, err := net.Listen("tcp", address)
@@ -93,13 +100,39 @@ func makeMjpegMuxer(inputAddr string, outputAddr string) {
 	http.HandleFunc(outputAddr, handleMjpegStreamRequest(mux))
 }
 
+func lauchImx219CsiCameraMjpegStream(index uint, width uint, height uint, rWidth uint, rHeight uint, quality uint, port uint) {
+	time.Sleep(100 * time.Millisecond)
+	cmdSetup := exec.Command(
+		gstpipeline.CsiCameraSetup(gstpipeline.IMX219, index, width, height),
+	)
+	_, err := cmdSetup.Output()
+	if err != nil {
+		log.Fatal("Cannot setup CsiCamera: ", err)
+	}
+	log.Print(cmdSetup.Args)
+	cmd := exec.Command(
+		gstpipeline.GStreamerLaunch(),
+		gstpipeline.CsiCameraSource(gstpipeline.IMX219, index, width, height),
+		gstpipeline.DecodeBinRescale(rWidth, rHeight),
+		gstpipeline.JpegTcpStreamLocalhost(quality, port, MJPEG_FRAME_BOUNDARY),
+	)
+	cmd.Stdout = log.Writer()
+	log.Print(cmd.Args)
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal("Cannot start GStreamer pipeline: ", err)
+	}
+}
+
 func main() {
 	// gst-launch-1.0 -v videotestsrc ! video/x-raw,width=640,height=480 ! jpegenc quality=80 ! multipartmux boundary=frameboundary ! tcpclientsink host=127.0.0.1 port=9990
 	// sudo gst-launch-1.0 v4l2src device=/dev/video2 ! video/x-bayer, width=1920, height=1080, format=rggb ! tiovxisp sink_0::device=/dev/v4l-subdev2 sensor-name=SENSOR_SONY_IMX219_RPI dcc-isp-file=/opt/imaging/imx219/dcc_viss_1920x1080.bin sink_0::dcc-2a-file=/opt/imaging/imx219/dcc_2a_1920x1080.bin format-msb=7 ! decodebin ! videoscale method=0 add-borders=false ! video/x-raw,width=1280,height=720 ! jpegenc quality=50 ! multipartmux boundary=frameboundary ! tcpclientsink host=127.0.0.1 port=9990
 	makeMjpegMuxer(":9990", "/mjpeg_stream1")
+	go lauchImx219CsiCameraMjpegStream(0, CAMERA_WIDTH, CAMERA_HEIGHT, RESCALE_WIDTH, RESCALE_HEIGHT, JPEG_QUALITY, 9990)
 
 	// sudo gst-launch-1.0 v4l2src device=/dev/video18 ! video/x-bayer, width=1920, height=1080, format=rggb ! tiovxisp sink_0::device=/dev/v4l-subdev5 sensor-name=SENSOR_SONY_IMX219_RPI dcc-isp-file=/opt/imaging/imx219/dcc_viss_1920x1080.bin sink_0::dcc-2a-file=/opt/imaging/imx219/dcc_2a_1920x1080.bin format-msb=7 ! decodebin ! videoscale method=0 add-borders=false ! video/x-raw,width=1280,height=720 ! jpegenc quality=50 ! multipartmux boundary=frameboundary ! tcpclientsink host=127.0.0.1 port=9991
 	makeMjpegMuxer(":9991", "/mjpeg_stream2")
+	go lauchImx219CsiCameraMjpegStream(1, CAMERA_WIDTH, CAMERA_HEIGHT, RESCALE_WIDTH, RESCALE_HEIGHT, JPEG_QUALITY, 9991)
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 
