d92b2a4239b02f07d6b5cd3943b26535b9d2b42d
illia <illia.pikin@gmail.com>
2023-11-25 19:40:39 +0200

    wip: combined stereo stream

--- go/cmd/rgba_stereo_to_mjpeg_http_mux/main.go
+++ go/cmd/rgba_stereo_to_mjpeg_http_mux/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bbai64/muxer"
-	"bufio"
 	"image"
 	"image/jpeg"
 	"io"
@@ -43,11 +42,10 @@ func serveTcpRgbaStreamSocketConnection(conn net.Conn, fWidth int, fHeight int, 
 	}
 
 	var buffIndex int32
-	reader := bufio.NewReader(conn)
 	for {
 		frame := buffer[buffIndex]
 		buffIndex = (buffIndex + 1) % BUFFERED_FRAMES_COUNT
-		size, err := reader.Read(frame.Pix)
+		size, err := io.ReadFull(conn, frame.Pix)
 		if err != nil {
 			if err == io.EOF {
 				log.Print("Socket connection closed at ", address)
@@ -64,34 +62,59 @@ func serveTcpRgbaStreamSocketConnection(conn net.Conn, fWidth int, fHeight int, 
 	}
 }
 
-func handleMjpegStreamRequest(mux *muxer.Muxer[image.RGBA]) func(w http.ResponseWriter, req *http.Request) {
+func handleMjpegStreamRequest(rWidth int, rHeight int, mux1 *muxer.Muxer[image.RGBA], mux2 *muxer.Muxer[image.RGBA]) func(w http.ResponseWriter, req *http.Request) {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		log.Print("HTTP Connection established with ", req.RemoteAddr)
 		rw.Header().Add("Content-Type", "multipart/x-mixed-replace; boundary=--"+MJPEG_FRAME_BOUNDARY)
 		boundary := "\r\n--" + MJPEG_FRAME_BOUNDARY + "\r\nContent-Type: image/jpeg\r\n\r\n"
 		jpegOpts := &jpeg.Options{Quality: JPEG_QUALITY}
-		client := muxer.NewClient(mux)
-		defer client.Close()
+
+		client1 := muxer.NewClient(mux1)
+		defer client1.Close()
+		client2 := muxer.NewClient(mux2)
+		defer client1.Close()
 		timer := time.NewTimer(CONNECTION_TIMEOUT)
 		defer timer.Stop()
-		var frame *image.RGBA
+
+		var frame1 *image.RGBA
+		var frame2 *image.RGBA
+		imgCombined := image.NewRGBA(image.Rect(0, 0, rWidth, rHeight))
 		var ok bool
 		for {
 			select {
 			case <-timer.C:
 				log.Print("Lost stream for ", req.RemoteAddr)
 				return
-			case frame, ok = <-client.Send:
+			case frame1, ok = <-client1.Send:
+				for len(client1.Send) != 0 {
+					frame1, ok = <-client1.Send
+				}
 			}
-			timer.Reset(CONNECTION_TIMEOUT)
 			if !ok {
 				break
 			}
+			select {
+			case <-timer.C:
+				log.Print("Lost stream for ", req.RemoteAddr)
+				return
+			case frame2, ok = <-client2.Send:
+				for len(client2.Send) != 0 {
+					frame2, ok = <-client2.Send
+				}
+			}
+			if !ok {
+				break
+			}
+			timer.Reset(CONNECTION_TIMEOUT)
+
+			copy(imgCombined.Pix[:len(frame1.Pix)], frame1.Pix)
+			copy(imgCombined.Pix[len(frame1.Pix):], frame2.Pix)
+
 			if n, err := io.WriteString(rw, boundary); err != nil || n != len(boundary) {
 				log.Print("Cannot write response to ", req.RemoteAddr)
 				break
 			}
-			if err := jpeg.Encode(rw, frame, jpegOpts); err != nil {
+			if err := jpeg.Encode(rw, imgCombined, jpegOpts); err != nil {
 				log.Print("Cannot write response to ", req.RemoteAddr)
 				break
 			}
@@ -100,21 +123,24 @@ func handleMjpegStreamRequest(mux *muxer.Muxer[image.RGBA]) func(w http.Response
 				break
 			}
 		}
-
 		log.Print("HTTP Connection closed with ", req.RemoteAddr)
 	}
 }
 
-func makeStereoCameraMuxer(inputAddr1 string, outputAddr string) {
+func makeStereoCameraMuxer(inputAddr1 string, inputAddr2 string, outputAddr string) {
 	mux1 := muxer.NewMuxer[image.RGBA](BUFFERED_FRAMES_COUNT)
+	mux2 := muxer.NewMuxer[image.RGBA](BUFFERED_FRAMES_COUNT)
 	go mux1.Run()
+	go mux2.Run()
 	// gst-launch-1.0 videotestsrc ! video/x-raw, width=640, height=480, format=NV12 ! videoconvert ! video/x-raw, format=RGBA ! tcpclientsink host=127.0.0.1 port=9990
 	go serveTcpRgbaStreamSocket(640, 480, mux1, inputAddr1)
-	http.HandleFunc(outputAddr, handleMjpegStreamRequest(mux1))
+	// gst-launch-1.0 videotestsrc ! video/x-raw, width=640, height=480, format=NV12 ! videoconvert ! video/x-raw, format=RGBA ! tcpclientsink host=127.0.0.1 port=9991
+	go serveTcpRgbaStreamSocket(640, 480, mux2, inputAddr2)
+	http.HandleFunc(outputAddr, handleMjpegStreamRequest(640, 480*2, mux1, mux2))
 }
 
 func main() {
-	makeStereoCameraMuxer(":9990", "/mjpeg_stream")
+	makeStereoCameraMuxer(":9990", ":9991", "/mjpeg_stream")
 
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 
