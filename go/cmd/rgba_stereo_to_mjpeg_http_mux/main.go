2fca9aafb028f370eb835858bfa977a29b1d13a9
illia <illia.pikin@gmail.com>
2023-11-30 21:08:03 +0200

    trying to use different jpeg encoder

--- go/cmd/rgba_stereo_to_mjpeg_http_mux/main.go
+++ go/cmd/rgba_stereo_to_mjpeg_http_mux/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"bbai64/gstpipeline"
 	"bbai64/muxer"
-	"image"
-	"image/jpeg"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"time"
+
+	"github.com/Hypnotriod/jpegenc"
 )
 
 const SERVER_ADDRESS = ":1337"
@@ -18,11 +18,13 @@ const MJPEG_FRAME_BOUNDARY = "frameboundary"
 const CONNECTION_TIMEOUT = 1 * time.Second
 const CAMERA_WIDTH = 1920
 const CAMERA_HEIGHT = 1080
-const RESCALE_WIDTH = 320  //1280
-const RESCALE_HEIGHT = 360 //720
+const RESCALE_WIDTH = 1280
+const RESCALE_HEIGHT = 720
 const JPEG_QUALITY = 80
 
-func serveTcpRgbaStreamSocket(width int, height int, mux *muxer.Muxer[image.RGBA], address string) {
+type PixelsRGB16 []byte
+
+func serveTcpRgbaStreamSocket(width int, height int, mux *muxer.Muxer[PixelsRGB16], address string) {
 	soc, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatal("Cannot open socket at ", address, " : ", err)
@@ -38,39 +40,43 @@ func serveTcpRgbaStreamSocket(width int, height int, mux *muxer.Muxer[image.RGBA
 	}
 }
 
-func serveTcpRgbaStreamSocketConnection(conn net.Conn, width int, height int, mux *muxer.Muxer[image.RGBA], address string) {
+func serveTcpRgbaStreamSocketConnection(conn net.Conn, width int, height int, mux *muxer.Muxer[PixelsRGB16], address string) {
 	log.Print("Accepted input stream at ", address)
 
-	buffer := [BUFFERED_FRAMES_COUNT]*image.RGBA{}
+	buffer := [BUFFERED_FRAMES_COUNT]PixelsRGB16{}
 	for i := range buffer {
-		buffer[i] = image.NewRGBA(image.Rect(0, 0, width, height))
+		buffer[i] = make(PixelsRGB16, width*height*2)
 	}
 
 	var buffIndex int32
 	for {
 		frame := buffer[buffIndex]
 		buffIndex = (buffIndex + 1) % BUFFERED_FRAMES_COUNT
-		size, err := io.ReadFull(conn, frame.Pix)
+		size, err := io.ReadFull(conn, frame)
 		if err != nil {
 			if err == io.EOF {
 				log.Print("Socket connection closed at ", address)
-			} else if size != len(frame.Pix) {
-				log.Print("Stream has wrong rgba frame size! Expected: ", len(frame.Pix), ", but got ", size)
+			} else if size != len(frame) {
+				log.Print("Stream has wrong rgba frame size! Expected: ", len(frame), ", but got ", size)
 			} else {
 				log.Print("Socket read error ", err)
 			}
 			break
 		}
-		mux.Broadcast <- frame
+		mux.Broadcast <- &frame
 	}
 }
 
-func handleMjpegStreamRequest(width int, height int, muxL *muxer.Muxer[image.RGBA], muxR *muxer.Muxer[image.RGBA]) func(w http.ResponseWriter, req *http.Request) {
+func handleMjpegStreamRequest(width int, height int, muxL *muxer.Muxer[PixelsRGB16], muxR *muxer.Muxer[PixelsRGB16]) func(w http.ResponseWriter, req *http.Request) {
 	return func(rw http.ResponseWriter, req *http.Request) {
 		log.Print("HTTP Connection established with ", req.RemoteAddr)
 		rw.Header().Add("Content-Type", "multipart/x-mixed-replace; boundary=--"+MJPEG_FRAME_BOUNDARY)
 		boundary := "\r\n--" + MJPEG_FRAME_BOUNDARY + "\r\nContent-Type: image/jpeg\r\n\r\n"
-		jpegOpts := &jpeg.Options{Quality: JPEG_QUALITY}
+		jpegParams := jpegenc.JpegEncodeParams{
+			QualityFactor: jpegenc.JpegQualityFactorBest,
+			PixelType:     jpegenc.JpegPixelTypeRGB565,
+			Subsample:     jpegenc.JpegSubsample444,
+		}
 
 		clientL := muxer.NewClient(muxL)
 		defer clientL.Close()
@@ -79,9 +85,10 @@ func handleMjpegStreamRequest(width int, height int, muxL *muxer.Muxer[image.RGB
 		timer := time.NewTimer(CONNECTION_TIMEOUT)
 		defer timer.Stop()
 
-		var frameL *image.RGBA
-		var frameR *image.RGBA
-		imgCombined := image.NewRGBA(image.Rect(0, 0, width, height))
+		var frameL *PixelsRGB16
+		var frameR *PixelsRGB16
+		mergedFrame := make(PixelsRGB16, width*height*2*2)
+		jpegBuffer := make([]byte, width*height)
 		var ok bool
 		for {
 			select {
@@ -110,20 +117,24 @@ func handleMjpegStreamRequest(width int, height int, muxL *muxer.Muxer[image.RGB
 			}
 			timer.Reset(CONNECTION_TIMEOUT)
 
-			copy(imgCombined.Pix[:len(frameL.Pix)], frameL.Pix)
-			copy(imgCombined.Pix[len(frameL.Pix):], frameR.Pix)
+			copy(mergedFrame[:len(*frameL)], *frameL)
+			copy(mergedFrame[len(*frameL):], *frameR)
 
 			if n, err := io.WriteString(rw, boundary); err != nil || n != len(boundary) {
-				log.Print("Cannot write response to ", req.RemoteAddr)
+				log.Print("Cannot write response to ", req.RemoteAddr, ": ", err)
 				break
 			}
-			// insanely terrible performance from go standard jpeg.Encode
-			if err := jpeg.Encode(rw, imgCombined, jpegOpts); err != nil {
-				log.Print("Cannot write response to ", req.RemoteAddr)
+			bytesEncoded, err := jpegenc.Encode(width, height, jpegParams, mergedFrame, jpegBuffer)
+			if err != nil {
+				log.Print("Cannot write response to ", req.RemoteAddr, ": ", err)
+				break
+			}
+			if n, err := rw.Write(jpegBuffer[:bytesEncoded]); n != bytesEncoded || err != nil {
+				log.Print("Cannot write response to ", req.RemoteAddr, ": ", err)
 				break
 			}
 			if n, err := io.WriteString(rw, "\r\n"); err != nil || n != 2 {
-				log.Print("Cannot write response to ", req.RemoteAddr)
+				log.Print("Cannot write response to ", req.RemoteAddr, ": ", err)
 				break
 			}
 		}
@@ -132,8 +143,8 @@ func handleMjpegStreamRequest(width int, height int, muxL *muxer.Muxer[image.RGB
 }
 
 func makeStereoCameraMuxer(inputAddrL string, inputAddrR string, outputAddr string) {
-	muxL := muxer.NewMuxer[image.RGBA](BUFFERED_FRAMES_COUNT)
-	muxR := muxer.NewMuxer[image.RGBA](BUFFERED_FRAMES_COUNT)
+	muxL := muxer.NewMuxer[PixelsRGB16](BUFFERED_FRAMES_COUNT)
+	muxR := muxer.NewMuxer[PixelsRGB16](BUFFERED_FRAMES_COUNT)
 	go muxL.Run()
 	go muxR.Run()
 	go serveTcpRgbaStreamSocket(RESCALE_WIDTH, RESCALE_HEIGHT, muxL, inputAddrL)
@@ -144,10 +155,10 @@ func makeStereoCameraMuxer(inputAddrL string, inputAddrR string, outputAddr stri
 func main() {
 	// open with stereocomb.html
 	makeStereoCameraMuxer(":9990", ":9991", "/mjpeg_stream")
-	// gst-launch-1.0 videotestsrc ! video/x-raw, width=640, height=480, format=NV12 ! videoconvert ! video/x-raw, format=RGBA ! tcpclientsink host=127.0.0.1 port=9990
+	// gst-launch-1.0 videotestsrc ! video/x-raw, width=640, height=480, format=NV12 ! videoconvert ! video/x-raw, format=RGB16 ! tcpclientsink host=127.0.0.1 port=9990
 	go gstpipeline.LauchImx219CsiCameraRgbaStream(
 		0, CAMERA_WIDTH, CAMERA_HEIGHT, RESCALE_WIDTH, RESCALE_HEIGHT, 9990)
-	// gst-launch-1.0 videotestsrc ! video/x-raw, width=640, height=480, format=NV12 ! videoconvert ! video/x-raw, format=RGBA ! tcpclientsink host=127.0.0.1 port=9991
+	// gst-launch-1.0 videotestsrc ! video/x-raw, width=640, height=480, format=NV12 ! videoconvert ! video/x-raw, format=RGB16 ! tcpclientsink host=127.0.0.1 port=9991
 	go gstpipeline.LauchImx219CsiCameraRgbaStream(
 		1, CAMERA_WIDTH, CAMERA_HEIGHT, RESCALE_WIDTH, RESCALE_HEIGHT, 9991)
 	http.Handle("/", http.FileServer(http.Dir("./public")))
--- go/go.mod
+++ go/go.mod
@@ -1,3 +1,5 @@
 module bbai64
 
-go 1.21.1
+go 1.21.4
+
+require github.com/Hypnotriod/jpegenc v0.0.0-20231130184854-9353e1e54983
