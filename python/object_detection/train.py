83ca1302f4f6a380ca4ab6a5e17bb4f32742fdb8
illia <illia.pikin@gmail.com>
2024-04-27 11:55:15 +0300

    refactoring

--- python/object_detection/train.py
+++ python/object_detection/train.py
@@ -146,37 +146,26 @@ def generate_pipeline_config():
         base_config = f.read()
 
     with open(config["pipeline_config_path"], "w") as f:
-        # Set labelmap path
         base_config = re.sub('label_map_path: ".*?"',
                              'label_map_path: "{}"'.format(config["labelmap_path"]), base_config)
-        # Set fine_tune_checkpoint path
         base_config = re.sub('fine_tune_checkpoint: ".*?"',
                              'fine_tune_checkpoint: "{}"'.format(config["fine_tune_checkpoint"]), base_config)
-        # Set train tf-record file path
         base_config = re.sub('(input_path: ".*?)(PATH_TO_BE_CONFIGURED/train)(.*?")',
                              'input_path: "{}"'.format(config["train_record_path"]), base_config)
-        # Set test tf-record file path
         base_config = re.sub('(input_path: ".*?)(PATH_TO_BE_CONFIGURED/eval)(.*?")',
                              'input_path: "{}"'.format(config["test_record_path"]), base_config)
-        # Set number of classes.
         base_config = re.sub('num_classes: [0-9]+',
                              'num_classes: {}'.format(len(config["classes"])), base_config)
-        # Set batch size
         base_config = re.sub('batch_size: [0-9]+',
                              'batch_size: {}'.format(config["batch_size"]), base_config)
-        # Set training steps
         base_config = re.sub('num_steps: [0-9]+',
                              'num_steps: {}'.format(config["num_steps"]), base_config)
-        # Set max detections per class
         base_config = re.sub('max_detections_per_class: [0-9]+',
                              'max_detections_per_class: {}'.format(config["max_detections_per_class"]), base_config)
-        # Set max total detections
         base_config = re.sub('max_total_detections: [0-9]+',
                              'max_total_detections: {}'.format(config["max_total_detections"]), base_config)
-        # Set max number of boxes
         base_config = re.sub('max_number_of_boxes: [0-9]+',
                              'max_number_of_boxes: {}'.format(config["max_number_of_boxes"]), base_config)
-        # Set fine-tune checkpoint type to detection
         base_config = re.sub('fine_tune_checkpoint_type: "classification"',
                              'fine_tune_checkpoint_type: "{}"'.format('detection'), base_config)
         f.write(base_config)
